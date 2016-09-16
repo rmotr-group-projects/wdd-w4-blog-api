b6a0664ed64e867a417d18b67a8dfc498a8e08eb
Phil <wright.phillip@gmail.com>
2016-09-16 00:41:09 +0000

    Add views, serializers, urls, and filters

--- blog_api/blog/views.py
+++ blog_api/blog/views.py
@@ -5,3 +5,44 @@ from blog.serializers import *
 
 
 # implement Views here
+class EntryViewSet(mixins.CreateModelMixin,
+                mixins.ListModelMixin,
+                mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin,
+                viewsets.GenericViewSet):
+    '''
+    A viewset that provides ___ actions for the Entry model
+    '''
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
+
+
+class BlogViewSet(mixins.CreateModelMixin,
+                mixins.ListModelMixin,
+                mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin,
+                viewsets.GenericViewSet):
+    '''
+    A viewset that provides ___ actions for the Blog model
+    '''
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+
+class AuthorViewSet(mixins.CreateModelMixin,
+                mixins.ListModelMixin,
+                mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin,
+                viewsets.GenericViewSet):
+    '''
+    A viewset that provides ___ actions for the Author model
+    '''
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('name',)
+    ordering_fields = ('id',)
--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third-party apps
-    # don't forget to add DRF app here
+    'rest_framework',
 
     # custom apps
     'blog',
@@ -127,5 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # configure DRF here
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
